04da5aacc996b8da4bb01a10e6e0c9c66cd926c3
NemesLaszlo <wow.laszlo@gmail.com>
2020-07-24 23:53:06 +0200

    EntityFrameworkCore and Database Setup

---
+++ Commander/Repository/CommanderContext.cs
@@ -0,0 +1,16 @@
+using Commander.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commander.Repository
+{
+    public class CommanderContext : DbContext
+    {
+        public CommanderContext(DbContextOptions<CommanderContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Command> Commands { get; set; }
+
+    }
+}
--- Commander/Startup.cs
+++ Commander/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +28,7 @@ namespace Commander
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CommanderContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("CommanderConnection")));
             services.AddControllers();
             services.AddScoped<ICommanderRepo, CommanderService>();
         }
