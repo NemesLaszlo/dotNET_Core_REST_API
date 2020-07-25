362ee8ebb4908bab52bd0eccd54a88239d4a3006
NemesLaszlo <wow.laszlo@gmail.com>
2020-07-25 18:26:03 +0200

    CommanderService implementation with the database

--- Commander/Service/CommanderService.cs
+++ Commander/Service/CommanderService.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Commander.Models;
 using Commander.Repository;
 
@@ -6,19 +7,21 @@ namespace Commander.Service
 {
     public class CommanderService : ICommanderRepo
     {
+        private readonly CommanderContext _context;
+
         public CommanderService(CommanderContext context)
         {
-
+            _context = context;
         }
 
         public IEnumerable<Command> GetAllCommands()
         {
-
+            return _context.Commands.ToList();
         }
 
         public Command GetCommandById(int id)
         {
-
+            return _context.Commands.FirstOrDefault(p => p.Id == id);
         }
     }
 }
